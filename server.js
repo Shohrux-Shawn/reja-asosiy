75aefd1f375cbbcdb59fd0b961801edb1179067e
SHAWN <shakh3973@gmail.com>
2025-06-08 17:19:24 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -3,32 +3,37 @@ const express = require("express");
 const app = express();
 const http = require('http');
 
-
-
-
-
-//1 : Kirish code
+// 1 : Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //html dan tradition form dan post qilingan narsani serverga oladi
+app.use(express.urlencoded({ extended: true }));
 
-//2 Session
-
-//3   Views code
-app.set("view", "views");
+// 3 Views code
+app.set("views", "views");           
 app.set("view engine", "ejs");
 
-//4 Routing code
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+});
 
-app.get("/", function (req, res){
-    res.end("<h1>Hello World</h1>");
+app.get("/", (req, res) => {
+  res.render("reja"); 
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function()  {
-    console.log(`Server is running on port ${PORT}`);
-
-
-    
+server.listen(PORT, function () {
+  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
